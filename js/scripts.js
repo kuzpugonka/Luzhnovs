ce4c5f2621327ea2cc6e712376cdd3a1943628b2
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2023-03-19 18:57:35 +0300

    add первые файлы
    сырая лента времени и неполный перечень персон

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+document.getElementById("main-action").onclick = function () {
+  document.getElementById("cars").scrollIntoView({behavior: "smooth"});
+}
+
+var buttons = document.getElementsByClassName("car-button");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("price").scrollIntoView({behavior: "smooth"});
+    }
+}
+
+document.getElementById("price-action").onclick = function () {
+    if (document.getElementById("name").value === "") {
+        alert("Заполните поле имя!");
+    } else if (document.getElementById("phone").value === "") {
+        alert("Заполните поле телефон!");
+    } else if (document.getElementById("car").value === "") {
+        alert("Заполните поле автомобиль!");
+    } else {
+        alert("Спасибо, мы свяжемся с вами в ближайшее время")
+    }
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let layer = document.querySelector(".price-img");
+  document.addEventListener("mousemove", (event) => {
+    layer.style.transform =
+      "translate3d(" +
+      (event.clientX * 0.4) / 7 +
+      "px," +
+      (event.clientY * 0.4) / 7 +
+      "px,0px)";
+  });
+
+  const elem = document.querySelector(".main");
+  document.addEventListener("scroll", () => {
+    elem.style.backgroundPositionX = "0" + 0.4 * window.pageYOffset + "px";
+  });
+});
+
+
